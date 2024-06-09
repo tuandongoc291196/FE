6a968312549ff524c28c3f6e7ba0e2da5db4bd98
tuandongoc291196 <tuandnse62052@fpt.edu.vn>
2024-06-09 11:49:15 +0700

    Add main layout for photography page

--- frontend/src/components/pages/Photographer/Photographer.tsx
+++ frontend/src/components/pages/Photographer/Photographer.tsx
@@ -7,7 +7,7 @@ const Photographer = () => {
     return (
         <div id='Photographer'>
             <div className="row">
-                <div className="col-haft cover-search">
+                <div className="col-left cover-search">
                     <div className="landing-search">
                         <h1 className="header-search">
                             Chụp ảnh cưới
@@ -27,44 +27,92 @@ const Photographer = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col-haft">
-                    <img src="https://cdn1.weddingwire.com/assets/img/home/bg_hero1.jpg" alt="" />
-                </div>
             </div>
             <div className="content">
                 <aside className='aside-bar'>
                     <div className="filter-component">
                         <legend className="filter-name">Chi phí</legend>
                         <ul className="filter-list">
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> Under $1,000</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $1,000 - $1,999</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $2,000 - $3,999</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $3,000 - $4,999</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $4,000+</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> Under $1,000</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $1,000 - $1,999</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $2,000 - $3,999</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $3,000 - $4,999</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $4,000+</li>
                         </ul>
                     </div>
                     <div className="filter-component">
                         <legend className="filter-name">Chi phí</legend>
                         <ul className="filter-list">
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> Under $1,000</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $1,000 - $1,999</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $2,000 - $3,999</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $3,000 - $4,999</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $4,000+</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> Under $1,000</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $1,000 - $1,999</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $2,000 - $3,999</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $3,000 - $4,999</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $4,000+</li>
                         </ul>
                     </div>
                     <div className="filter-component">
                         <legend className="filter-name">Chi phí</legend>
                         <ul className="filter-list">
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> Under $1,000</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $1,000 - $1,999</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $2,000 - $3,999</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $3,000 - $4,999</li>
-                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': {fontSize: 18}}}/> $4,000+</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> Under $1,000</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $1,000 - $1,999</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $2,000 - $3,999</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $3,000 - $4,999</li>
+                            <li className="filter-item"><Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} /> $4,000+</li>
                         </ul>
                     </div>
                 </aside>
-                <div className="filter-content">Main layout</div>
+                <div className="filter-content">
+                    <div className="content-header"><strong>80</strong> results</div>
+                    <ul className="content-list">
+                        <li className="content-item">
+                            <div className="content-gallery">
+                                <img src="https://cdn0.weddingwire.com/vendor/262779/3_2/640/png/ww-storefront_51_977262-167476398667285.webp" alt="" />
+                            </div>
+                            <div className="content-infor">
+                                <h2 className="item-title">Sound Originals Photo & Video</h2>
+                                <div className="item-subtitle">
+                                    <div className="item-rating">5.0</div>
+                                    <span className="item-location">San Francisco, CA</span>
+                                </div>
+
+                                <p className="item-description">
+                                    <span>Sound Originals Photo & Video is a 5-star photography and videography team.
+                                        Couples love Sound Originals!
+                                        Storytelling</span>
+                                    <span className="read-more"> Lasts Forever.
+                                        Y dedicated to excellent quality. Artistic portraits.Y dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. ArtiY dedicated to excellent quality. Arti</span>
+                                </p>
+                                <div className="item-footer">
+                                    <Button className="btn-item" variant="contained">Request pricing</Button>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="content-item">
+                            <div className="content-gallery">
+                                <img src="https://cdn0.weddingwire.com/vendor/262779/3_2/640/png/ww-storefront_51_977262-167476398667285.webp" alt="" />
+                            </div>
+                            <div className="content-infor">
+                                <h2 className="item-title">Sound Originals Photo & Video</h2>
+                                <div className="item-subtitle">
+                                    <div className="item-rating">5.0</div>
+                                    <span className="item-location">San Francisco, CA</span>
+                                </div>
+
+                                <p className="item-description">
+                                    <span>Sound Originals Photo & Video is a 5-star photography and videography team.
+                                        Couples love Sound Originals!
+                                        Storytelling</span>
+                                    <span className="read-more"> Lasts Forever.
+                                        Your story deserves beautiful photos and videos you absolutely love. Sound Originals is a small team of artists dedicated to excellent quality. Artistic portraits.</span>
+                                </p>
+                                <div className="item-footer">
+                                    <div></div>
+                                    <Button className="btn-item" variant="contained">Request pricing</Button>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )

a7936f79ccc047a2cd1b2b9a477e533ddfe69929
tuandongoc291196 <tuandongoc291196@gmail.com>
2024-07-03 22:09:43 +0700

    Remove local url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,5 +2,4 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: 'https://weddingapp.whiteflower-8a918537.australiaeast.azurecontainerapps.io'
-    // baseURL: 'http://localhost:8080/'
 });

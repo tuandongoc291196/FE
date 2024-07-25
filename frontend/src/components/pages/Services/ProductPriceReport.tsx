2026fb86fadc3b581c53baee59f402407fb25537
tuandongoc291196 <tuandongoc291196@gmail.com>
2024-07-25 22:53:55 +0700

    Add title page

--- frontend/src/components/pages/Services/ProductPriceReport.tsx
+++ frontend/src/components/pages/Services/ProductPriceReport.tsx
@@ -10,85 +10,83 @@ import "./Services.css";
 
 
 interface Props {
-  setMessageStatus: Dispatch<SetStateAction<string>>;
-  setMessage: Dispatch<SetStateAction<string>>;
+    setMessageStatus: Dispatch<SetStateAction<string>>;
+    setMessage: Dispatch<SetStateAction<string>>;
 }
 
 
 const ProductPriceReporter: FC<Props> = (props) => {
-  const [reporters, setReporters] = useState<ProductPriceReporterEntity[]>([]);
+    const [reporters, setReporters] = useState<ProductPriceReporterEntity[]>([]);
 
 
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-  useEffect(() => {
-    const reportersList: ProductPriceReporterEntity[] = [
-      {
-        "id": "1",
-        "coupleName": "Couple 1",
-        "categoryName": "Category 1",
-        "serviceName": "Service 1",
-        "requirement": "Requirement",
-        "priceReport": "30000000",
-      },
-      {
-        "id": "2",
-        "coupleName": "Couple 2",
-        "categoryName": "Category 2",
-        "serviceName": "Service 2",
-        "requirement": "Requirement",
-        "priceReport": "30000000",
-      }
+    useEffect(() => {
+        const reportersList: ProductPriceReporterEntity[] = [
+            {
+                "id": "1",
+                "coupleName": "Couple 1",
+                "categoryName": "Category 1",
+                "serviceName": "Service 1",
+                "requirement": "Requirement",
+                "priceReport": "30000000",
+            },
+            {
+                "id": "2",
+                "coupleName": "Couple 2",
+                "categoryName": "Category 2",
+                "serviceName": "Service 2",
+                "requirement": "Requirement",
+                "priceReport": "30000000",
+            }
+        ];
+
+        setReporters(reportersList);
+    }, [])
+
+    const rows = reporters?.length > 0 ? reporters.map((reporter) => ({
+        id: reporter.id,
+        coupleName: reporter.coupleName,
+        categoryName: reporter.categoryName,
+        serviceName: reporter.serviceName,
+        requirement: reporter.requirement,
+        priceReport: reporter.priceReport
+    })) : [];
+
+    const columns: GridColDef[] = [
+        { field: "id", headerName: "ID", flex: 0.3 },
+        { field: "coupleName", headerName: "Tên Couple", flex: 0.5 },
+        { field: "categoryName", headerName: "Loại", flex: 0.8 },
+        { field: "serviceName", headerName: "Dịch vụ", flex: 1.2 },
+        { field: "requirement", headerName: "Yêu cầu", flex: 0.5 },
+        { field: "priceReport", headerName: "Báo giá", flex: 0.5 },
+        // {
+        //   field: '',
+        //   headerName: 'Tác vụ',
+        //   flex: 0.5,
+        //   width: 170,
+        //   renderCell: (params) => (
+        //     <div className="action">
+        //       <VisibilityIcon onClick={() => navigate(`/service-detail/${params.id}`)}></VisibilityIcon>
+        //       <AppRegistrationIcon></AppRegistrationIcon>
+        //       <DeleteIcon></DeleteIcon>
+        //     </div>
+        //   ),
+        // }
     ];
 
-    setReporters(reportersList);
-  }, [])
-
-  const rows = reporters?.length > 0 ? reporters.map((reporter) => ({
-    id: reporter.id,
-    coupleName: reporter.coupleName,
-    categoryName: reporter.categoryName,
-    serviceName: reporter.serviceName,
-    requirement: reporter.requirement,
-    priceReport: reporter.priceReport
-  })) : [];
-
-  const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", flex: 0.3 },
-    { field: "coupleName", headerName: "Tên Couple", flex: 0.5 },
-    { field: "categoryName", headerName: "Loại", flex: 0.8 },
-    { field: "serviceName", headerName: "Dịch vụ", flex: 1.2 },
-    { field: "requirement", headerName: "Yêu cầu", flex: 0.5 },
-    { field: "priceReport", headerName: "Báo giá", flex: 0.5 },
-    // {
-    //   field: '',
-    //   headerName: 'Tác vụ',
-    //   flex: 0.5,
-    //   width: 170,
-    //   renderCell: (params) => (
-    //     <div className="action">
-    //       <VisibilityIcon onClick={() => navigate(`/service-detail/${params.id}`)}></VisibilityIcon>
-    //       <AppRegistrationIcon></AppRegistrationIcon>
-    //       <DeleteIcon></DeleteIcon>
-    //     </div>
-    //   ),
-    // }
-  ];
-
-  return (
-    <div id="Services">
-      <div className="create-service">
-        <Button className="btn-create-service" onClick={() => { handleOpen() }}>Tạo mới</Button>
-      </div>
-      <div className="table" style={{ height: 400, width: "100%" }}>
-        <DataGrid rows={rows}
-          columns={columns}
-          autoPageSize
-          pagination />
-      </div>
-      {/* <Modal
+    return (
+        <div id="Services">
+            <h2 className="h2-title-page" >Báo giá</h2>
+            <div className="table" style={{ height: 400, width: "100%" }}>
+                <DataGrid rows={rows}
+                    columns={columns}
+                    autoPageSize
+                    pagination />
+            </div>
+            {/* <Modal
                 open={open}
                 onClose={handleClose}
                 aria-labelledby="modal-modal-title"
@@ -171,8 +169,8 @@ const ProductPriceReporter: FC<Props> = (props) => {
                     </Typography>
                 </Box>
             </Modal> */}
-    </div>
-  )
+        </div>
+    )
 }
 
 export default ProductPriceReporter

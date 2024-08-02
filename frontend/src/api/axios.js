36b1ec6a656703617a66a28f6e0cba4492f56717
LyDuy284 <lyhieuduy9190@gmail.com>
2024-08-02 18:50:49 +0700

    coupleapigetlistservice

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://weddingapp.whiteflower-8a918537.australiaeast.azurecontainerapps.io'
-    // baseURL: 'http://localhost:8080/'
+    // baseURL: 'https://weddingapp.whiteflower-8a918537.australiaeast.azurecontainerapps.io'
+    baseURL: 'http://localhost:8080/'
 });
